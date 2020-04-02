d7edd2d4737ec03d6e98960afe177e6b7c3b0b13
Chris Walter <walt50@gmail.com>
2020-04-01 22:03:34 -0400

    Stock API added. Alpha Vantage

--- APIApp/FOffVC.swift
+++ APIApp/FOffVC.swift
@@ -33,40 +33,28 @@ class FOffVC: UIViewController {
             name = "Doc"
         }
         let urlString = "http://foaas.com/programmer/\(name)"
-                if let url = URL(string: urlString)
-                {
-                    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                                
-                         guard let data = data else { return }
-                         let result = String(data: data, encoding: .utf8)! // this is the String of the data. // The result came up as HTML...
-                         print(String(data: data, encoding: .utf8)!)
+        if let url = URL(string: urlString)
+        {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                         
-                                
-                         // do stuff with the data
-                         DispatchQueue.main.async {
-                              // do stuff to the ui like update a label etc.  All that must be done on the main thread.
-                            
-                            // this snippet will convert HTML data to attributed string.
-                            if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-                                self.resultLabel.attributedText = attributedString
-                            }
-        //                    self.resultLabel.text = result
-                         }
-                     }
-                     task.resume()
+                 guard let data = data else { return }
+                 let result = String(data: data, encoding: .utf8)! // this is the String of the data. // The result came up as HTML...
+                 print(String(data: data, encoding: .utf8)!)
+                
+                        
+                 // do stuff with the data
+                 DispatchQueue.main.async {
+                      // do stuff to the ui like update a label etc.  All that must be done on the main thread.
+                    
+                    // this snippet will convert HTML data to attributed string.
+                    if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+                        self.resultLabel.attributedText = attributedString
+                    }
+//                    self.resultLabel.text = result
                  }
-        
+             }
+             task.resume()
+         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
