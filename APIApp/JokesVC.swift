76b049604a67b3d6dbf2af5d60b6d4b8389f9d0a
cwalter50 <walt50@gmail.com>
2020-09-22 22:38:26 -0400

    updated Jokes API

--- APIApp/JokesVC.swift
+++ APIApp/JokesVC.swift
@@ -32,15 +32,16 @@ class JokesVC: UIViewController
             let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
                 guard let data = data else { return }
                 do {
+                    // the data is JSON. This will convert it to a dictionary so that we can grab individual parts
                     let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]
-                    
+                    print(json as Any)
+                    // grab the values from the dictionary
                     let setup = json!["setup"] as? String ?? "setup Error"
                     let punchline = json!["punchline"] as? String ?? "punchline Error"
                     DispatchQueue.main.async {
+                        // do stuff to the ui like update a label etc.  All that must be done on the main thread.
                         self.displayLabel.text = "\(setup)\n\n...\(punchline)"
-                        
                     }
-                    
                 } catch let jsonError {
                     print(jsonError)
                 }
